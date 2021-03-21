fa8ea6957c6b926d26f1063dcb18ae1a16574476
Yehouda <yehouda_arnauve@hotmail.com>
2021-03-21 19:29:28 +0100

    refactoring buzz returns

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,10 @@ package com.kata.fizzbuzz;
 public class FizzBuzz {
 
     public static String isFizzBuzzNumber(int number) {
-        if (number == 5 || number == 10 || number == 15) {
-            return "Buzz";
-        }
-        return "Fizz";
+        return isDivisibleBy5(number) ? "Buzz" : "Fizz";
+    }
+
+    private static boolean isDivisibleBy5(int number) {
+        return number % 5 == 0;
     }
 }
--- src/test/java/com/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/kata/fizzbuzz/FizzBuzzTest.java
@@ -2,6 +2,8 @@ package com.kata.fizzbuzz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class FizzBuzzTest {
 
@@ -14,28 +16,9 @@ class FizzBuzzTest {
         Assertions.assertEquals("Fizz", result);
     }
 
-    @Test
-    void should_return_buzz_when_number_is_5() {
-        int number = 5;
-
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("Buzz", result);
-    }
-
-    @Test
-    void should_return_buzz_when_number_is_10() {
-        int number = 10;
-
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("Buzz", result);
-    }
-
-    @Test
-    void should_return_buzz_when_number_is_15() {
-        int number = 10;
-
+    @ParameterizedTest(name = "#{index} - should return buzz when number is {0}")
+    @ValueSource(ints = {5, 10, 15, 20, 25})
+    void should_return_buzz_when_number_is_divisible_by_5(int number) {
         String result = FizzBuzz.isFizzBuzzNumber(number);
 
         Assertions.assertEquals("Buzz", result);
