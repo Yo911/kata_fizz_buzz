9a706707608639f66ae796e6bab765f81b4b3b26
Yehouda <yehouda_arnauve@hotmail.com>
2021-03-21 19:39:27 +0100

    refactoring fizzbuzz returns

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -3,10 +3,14 @@ package com.kata.fizzbuzz;
 public class FizzBuzz {
 
     public static String isFizzBuzzNumber(int number) {
-        if (number == 15 || number == 30) {
+        if (isDivisibleBy3AndBy5(number)) {
             return "FizzBuzz";
         }
         return isDivisibleBy5(number) ? "Buzz" : "Fizz";
+    }
+
+    private static boolean isDivisibleBy3AndBy5(int number) {
+        return number % 5 == 0 && number % 3 == 0;
     }
 
     private static boolean isDivisibleBy5(int number) {
