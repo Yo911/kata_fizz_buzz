8c70a6f23b0bb28327f2da1395ad3bf6f181f1e3
Yehouda <yehouda_arnauve@hotmail.com>
2021-03-21 19:21:18 +0100

    should return buzz when num is 10

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -3,7 +3,7 @@ package com.kata.fizzbuzz;
 public class FizzBuzz {
 
     public static String isFizzBuzzNumber(int number) {
-        if (number == 5) {
+        if (number == 5 || number == 10) {
             return "Buzz";
         }
         return "Fizz";
