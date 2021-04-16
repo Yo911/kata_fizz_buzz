897cc0529704e355430caa72b5dbd6204482a8df
Yehouda <yehouda_arnauve@hotmail.com>
2021-04-16 15:25:09 +0200

    add fractions and get lowest

--- src/main/java/com/kata/fractions/Fraction.java
+++ src/main/java/com/kata/fractions/Fraction.java
@@ -23,13 +23,34 @@ public class Fraction {
         return b == 0 ? a : gcd(b, a % b);
     }
 
+    void lowest()
+    {
+        // Finding gcd of both terms
+        int common_factor = gcd(this.numerator, this.denominator);
+
+        // Converting both terms into simpler
+        // terms by dividing them by common factor
+        this.denominator = this.denominator/common_factor;
+        this.numerator = this.numerator/common_factor;
+    }
+
     public void add(Fraction fraction2) {
-        if (fraction2.numerator == 10) {
-            this.numerator = 15;
-            this.denominator = 15;
-            return;
-        }
-        this.numerator = 10;
-        this.denominator = 15;
+        int gcd = gcd(this.denominator, fraction2.denominator);
+        Fraction fractionCommon = new Fraction(
+                this.numerator / gcd,
+                this.denominator / gcd
+        );
+        fractionCommon.denominator = this.denominator * fraction2.denominator / fractionCommon.denominator;
+
+        int numerator1 = this.numerator * (fractionCommon.denominator / this.denominator);
+        int numerator2 = fraction2.numerator * (fractionCommon.denominator / fraction2.denominator);
+        fractionCommon.numerator = numerator1 + numerator2;
+
+        this.numerator = fractionCommon.numerator;
+        this.denominator = fractionCommon.denominator;
+
+        gcd = gcd(this.numerator, this.denominator);
+        this.denominator = this.denominator / gcd;
+        this.numerator = this.numerator / gcd;
     }
 }
--- src/test/java/com/kata/fractions/FractionTest.java
+++ src/test/java/com/kata/fractions/FractionTest.java
@@ -6,36 +6,47 @@ import org.junit.jupiter.api.Test;
 class FractionTest {
 
     @Test
-    void should_return_10_on_15_when_add_fractions_5_on_15_and_5_on_15()
+    void should_return_2_on_3_when_add_fractions_5_on_15_and_5_on_15()
     {
         // Given
         Fraction fraction = new Fraction(5, 15);
         Fraction fraction2 = new Fraction(5, 15);
-        // fraction + fraction2 = 10/15
-        Fraction resultExpected = new Fraction(2, 3);
 
         // When
         fraction.add(fraction2);
 
         // Then
-        Assertions.assertEquals(10, fraction.getNumerator());
-        Assertions.assertEquals(15, fraction.getDenominator());
+        Assertions.assertEquals(2, fraction.getNumerator());
+        Assertions.assertEquals(3, fraction.getDenominator());
     }
 
     @Test
-    void should_return_15_on_15_when_add_fractions_5_on_15_and_10_on_15()
+    void should_return_1_on_1_when_add_fractions_5_on_15_and_10_on_15()
     {
         // Given
         Fraction fraction = new Fraction(5, 15);
         Fraction fraction2 = new Fraction(10, 15);
-        // fraction + fraction2 = 1
 
         // When
         fraction.add(fraction2);
 
         // Then
-        Assertions.assertEquals(15, fraction.getNumerator());
-        Assertions.assertEquals(15, fraction.getDenominator());
+        Assertions.assertEquals(1, fraction.getNumerator());
+        Assertions.assertEquals(1, fraction.getDenominator());
     }
 
+    @Test
+    void should_return_1_on_1_when_add_fractions_3_on_18_and_6_on_21()
+    {
+        // Given
+        Fraction fraction = new Fraction(3, 18);
+        Fraction fraction2 = new Fraction(6, 21);
+
+        // When
+        fraction.add(fraction2);
+
+        // Then
+        Assertions.assertEquals(3, fraction.getNumerator());
+        Assertions.assertEquals(7, fraction.getDenominator());
+    }
 }
