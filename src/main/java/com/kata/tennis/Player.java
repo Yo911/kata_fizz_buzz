36e6ef21e3980a2af65fe4c34302f59330813216
Yehouda <yehouda_arnauve@hotmail.com>
2021-05-04 11:11:35 +0200

    get score from player when start

--- src/main/java/com/kata/tennis/Player.java
+++ src/main/java/com/kata/tennis/Player.java
@@ -1,4 +1,7 @@
 package com.kata.tennis;
 
 public class Player {
+    public String getScore() {
+        return "0";
+    }
 }
--- src/test/java/com/kata/tennis/PlayerTest.java
+++ src/test/java/com/kata/tennis/PlayerTest.java
@@ -1,7 +1,21 @@
 package com.kata.tennis;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class PlayerTest {
+    private Player player;
 
+    @BeforeEach
+    void init() {
+        player = new Player();
+    }
+
+    @Test
+    void should_return_0_when_get_score() {
+        String score = player.getScore();
+
+        Assertions.assertEquals("0", score);
+    }
 }
