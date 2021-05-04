f1e463ca5d55fd19a47047baf2db2474de7bbb45
Yehouda <yehouda_arnauve@hotmail.com>
2021-05-04 11:27:02 +0200

    get score from game

--- src/main/java/com/kata/tennis/Game.java
+++ src/main/java/com/kata/tennis/Game.java
@@ -1,4 +1,16 @@
 package com.kata.tennis;
 
 public class Game {
+    private Player playerOne;
+
+    private Player playerTwo;
+
+    public Game(Player playerOne, Player playerTwo) {
+        this.playerOne = playerOne;
+        this.playerTwo = playerTwo;
+    }
+
+    public String showScore() {
+        return playerOne.getScore() + " : " + playerTwo.getScore();
+    }
 }
---
+++ src/test/java/com/kata/tennis/GameTest.java
@@ -0,0 +1,35 @@
+package com.kata.tennis;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    private Game game;
+
+    private Player playerOne;
+    private Player playerTwo;
+
+    @BeforeEach
+    void init() {
+        playerOne = new Player();
+        playerTwo = new Player();
+        game = new Game(playerOne, playerTwo);
+    }
+
+    @Test
+    void should_return_0_to_0_when_game_start() {
+        String score = game.showScore();
+
+        Assertions.assertEquals("0 : 0", score);
+    }
+
+    @Test
+    void should_return_love_to_0_when_player_have_one_point() {
+        playerOne.addPoint();
+        String score = game.showScore();
+
+        Assertions.assertEquals("Love : 0", score);
+    }
+}
