39abbd43feb74723763c24d814cd2fdca1671e09
Yehouda <yehouda_arnauve@hotmail.com>
2021-05-05 18:25:04 +0200

    clean for restart

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -1,26 +1,4 @@
 package com.kata.fizzbuzz;
 
 public class FizzBuzz {
-
-    public static String isFizzBuzzNumber(int number) {
-        if (isDivisibleBy3AndBy5(number)) {
-            return "FizzBuzz";
-        }
-        if (isDivisibleBy3(number)) {
-            return "Fizz";
-        }
-        return isDivisibleBy5(number) ? "Buzz" : "";
-    }
-
-    private static boolean isDivisibleBy3(int number) {
-        return number % 3 == 0;
-    }
-
-    private static boolean isDivisibleBy3AndBy5(int number) {
-        return isDivisibleBy3(number) && isDivisibleBy5(number);
-    }
-
-    private static boolean isDivisibleBy5(int number) {
-        return number % 5 == 0;
-    }
 }
--- src/test/java/com/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/com/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,54 +1,5 @@
 package com.kata.fizzbuzz;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class FizzBuzzTest {
-
-    @Test
-    void should_return_fizz_when_number_is_3() {
-        int number = 3;
-
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("Fizz", result);
-    }
-
-    @Test
-    void should_return_fizz_when_number_is_6() {
-        int number = 6;
-
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("Fizz", result);
-    }
-
-    @ParameterizedTest(name = "#{index} - should return buzz when number is {0}")
-    @ValueSource(ints = {5, 10, 20, 25})
-    void should_return_buzz_when_number_is_divisible_by_5(int number) {
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("Buzz", result);
-    }
-
-    @ParameterizedTest(name = "#{index} - should return fizzbuzz when number is {0}")
-    @ValueSource(ints = {15, 30, 45})
-    void should_return_fizzbuzz_when_number_is_divisible_by_5_and_by_3() {
-        int number = 15;
-
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("FizzBuzz", result);
-    }
-
-    @Test
-    void should_return_empty_string_when_number_is_4() {
-        int number = 4;
-
-        String result = FizzBuzz.isFizzBuzzNumber(number);
-
-        Assertions.assertEquals("", result);
-    }
 }
