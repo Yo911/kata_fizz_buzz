c704a1fecbb4ce693b7de6de25c0858208fa3d68
Yehouda <yehouda_arnauve@hotmail.com>
2021-05-05 18:27:12 +0200

    rules fizz buzz

--- src/main/java/com/kata/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/kata/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,15 @@
 package com.kata.fizzbuzz;
 
 public class FizzBuzz {
+    /**
+     * FIZZ BUZZ JAZZ :
+     * Fizz when number is divisible 3
+     * Buzz when number is divisible 5
+     * Jazz when number is divisible 7
+     * FizzBuzz when number is divisible 3 & 5
+     * FizzJazz when number is divisible 3 & 7
+     * BuzzJazz when number is divisible 5 & 7
+     * FizzBuzzJazz when number is divisible 3 & 5 & 7
+     * return the number when it's not divisible by 3, 5 or 7
+     */
 }
